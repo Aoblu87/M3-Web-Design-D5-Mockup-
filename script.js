ea740ba127e388d6e66354d3e15e65eb115f7c30
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-08-25 21:53:17 +0200

    completata funzione. problema css, quando esco dalla card la selezione non si vede

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const bookmarks=document.querySelectorAll('[data-target=favorites]')
-
+const bookmarkFill=document.querySelectorAll('.bi-bookmark-fill')
 
 window.onload= function(){
 
 for (const bookmark of bookmarks) {
     bookmark.addEventListener('click', function(event){
 
-        bookmark.classList.toggle('favorites')
+        for (const fill of bookmarkFill) {
+            
+            fill.classList.toggle('favorites')
+        }
+
 
     })
 }
