e77a359c304ecb0caef1680bd5c3a207a12791c6
Stefania <126720391+Aoblu87@users.noreply.github.com>
2024-03-29 17:58:17 +0100

    stilato navbar- problema con svg

---
+++ script/floatingButton.js
@@ -0,0 +1,54 @@
+import { arrow } from '.././node_modules/@floating-ui/core';
+import { offset } from '.././node_modules/@floating-ui/dom';
+import { flip, shift } from '@floating-ui/dom';
+const { computePosition } = FloatingUIDOM;
+const button = document.querySelector('#button');
+const tooltip = document.querySelector('#tooltip');
+const arrowElement = document.querySelector('#arrow');
+
+function update() {
+  computePosition(button, tooltip, {
+    placement: 'top',
+    middleware: [
+      offset(6),
+      flip(),
+      shift({ padding: 5 }),
+      arrow({ element: arrowElement }),
+    ],
+  }).then(({ x, y, placement, middlewareData }) => {
+    Object.assign(tooltip.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
+    // Accessing the data
+    const { x: arrowX, y: arrowY } = middlewareData.arrow;
+    const staticSide = {
+      top: 'bottom',
+      right: 'left',
+      bottom: 'top',
+      left: 'right',
+    }[placement.split('-')[0]];
+
+    Object.assign(arrowElement.style, {
+      left: arrowX != null ? `${arrowX}px` : '',
+      top: arrowY != null ? `${arrowY}px` : '',
+      right: '',
+      bottom: '',
+      [staticSide]: '-4px',
+    });
+  });
+}
+function showTooltip() {
+  tooltip.style.display = 'block';
+  update();
+}
+function hideTooltip() {
+  tooltip.style.display = '';
+}
+
+function toggleTooltip() {
+  tooltip.style.display = tooltip.style.display === 'block' ? '' : 'block';
+  update();
+}
+
+button.addEventListener('click', toggleTooltip);
